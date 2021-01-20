94344e73fd0d6ab4d9ee107218fd754fd137a942
Davi Garcia de Souza <davigs@hotmail.com>
2021-01-20 17:43:36 -0300

    adicionado componente header

--- src/Pages/CartPage/index.js
+++ src/Pages/CartPage/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useHistory } from "react-router-dom";
-import { goBack, goToCartPage, goToHomePage, goToProfilePage, goToRestaurantsPage } from '../../Routes/coordinators';
+import Header from '../../Components/Header';
+import { goBack, goToHomePage, goToProfilePage, goToRestaurantsPage } from '../../Routes/coordinators';
+import GlobalStateContext from "../../Global/GlobalStateContext";
 
 
 export default function CartPage() {
     const history = useHistory();
+    const {states, setters} = useContext(GlobalStateContext)
+
+    useEffect(() => {
+        setters.setHeaderButton(false)
+    }, [])
 
     return (
         <div>
+            <Header pageName='Meu carrinho' />
             carrinho
 
             <button onClick={() => goToHomePage(history)}>
