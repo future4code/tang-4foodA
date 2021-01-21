0fca3bbd104c71c079180eb0e55b9dfeaa1c2666
Adrielly Felix <adrielly.jus.lex@hotmail.com>
2021-01-21 17:21:12 -0300

    tela-login

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { ContainerButton, CampoButton} from './styles'
+import React from "react";
+import { ContainerButton, CampoButton } from "./styles";
 
 export default function Buttons(props) {
-    return (
-        <CampoButton>
-          <ContainerButton 
-          type="submit"
-          color="primary" 
-          variant="contained"
-          onClick={props.submeter}>
-            {props.texto}
-          </ContainerButton>
-        </CampoButton>
-    )
+  return (
+    <CampoButton>
+      <ContainerButton
+        type="submit"
+        color="primary"
+        variant="contained"
+        onClick={props.submeter}
+      >
+        {props.texto}
+      </ContainerButton>
+    </CampoButton>
+  );
 }
--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -2,14 +2,17 @@ import styled from "styled-components";
 import Button from "@material-ui/core/Button";
 
 export const ContainerButton = styled(Button)`
-width: 20.5rem;
-display: flex;
-align-items: center;
-
-`
+  width: 20.5rem;
+  display: flex;
+  align-items: center;
+  color: #000;
+  font-weight: bold;
+  font-size: 1rem;
+  letter-spacing: -0.39px;
+  text-transform: normal;
+`;
 
 export const CampoButton = styled.div`
-text-align:center;
-
- 
-`
+  text-align: center;
+  margin: 1rem;
+`;
