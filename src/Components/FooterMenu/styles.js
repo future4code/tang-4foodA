8a73232a3dc8e2f010f64dae071efc3761f47793
Davi Garcia de Souza <davigs@hotmail.com>
2021-01-22 11:59:01 -0300

    finalizado componente FooterMenu

--- src/Components/FooterMenu/styles.js
+++ src/Components/FooterMenu/styles.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div `
-    
+    display: flex;
+    width: 100vw;
+    justify-content: space-evenly;
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.25);
+    height: 3rem;
+button {
+    flex-grow: 1;
+    padding: 0;
+    height: 3rem;
+    border: none;
+    background: none;
+}
 `
