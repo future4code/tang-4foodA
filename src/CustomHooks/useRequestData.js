0596c11a39ab316325b66a7f9202d4bc712875d8
David Biolchini <biolchinid@gmail.com>
2021-01-26 16:20:44 -0300

    colocando o token da aplicação no Request dos restaurantes

--- src/CustomHooks/useRequestData.js
+++ src/CustomHooks/useRequestData.js
@@ -11,7 +11,7 @@ export default function useRequestData(url,header, initialState) {
        }).catch((error) => {
            console.log(error.message)
        })
-   }, [url])
+   }, [url,header])
   
     return data
 }
